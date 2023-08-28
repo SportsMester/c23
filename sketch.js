81ce30ce5d0442363630f1bef60a4f9f6f797b83
SportsMester <138522423+SportsMester@users.noreply.github.com>
2023-08-28 15:54:06 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,34 +32,50 @@ function setup(){
     World.add(world,block1);
     
     //crie corpo block2 e adicione ao mundo
-    block2=Bodies.rectangle(150,450,150,20,plane_options);
+    block2=Bodies.rectangle(400,400,150,20,plane_options);
     World.add(world,block2);
 
     //crie particle_options
-    
+    var particle_options = {
+      restitution:0.4,
+      friction:0.02
+    }
     //crie o corpo particle1 e adicione ao mundo
     particle1 = Bodies.circle(220,10,10,particle_options);
     World.add(world,particle1);
     
     //crie o corpo particle2 e adicione ao mundo
-    
-    //crie o corpo particle3 e adicione ao mundo
-    
-    //crie o corpo particle4 e adicione ao mundo
-    
-    //crie o corpo particle5 e adicione ao mundo
+    particle2 = Bodies.circle(220,10,10,particle_options);
+    World.add(world,particle2);
 
+    //crie o corpo particle3 e adicione ao mundo
+    particle3 = Bodies.circle(220,10,10,particle_options);
+    World.add(world,particle3);
+
+    //crie o corpo particle4 e adicione ao mundo
+    particle4 = Bodies.circle(220,10,10,particle_options);
+    World.add(world,particle4);
+
+    //crie o corpo particle5 e adicione ao mundo
+    particle5 = Bodies.circle(220,10,10,particle_options);
+    World.add(world,particle5);
     
     //crie rotator_options
+    var rotator_options = {
+      isStatic: true,
+    }
 
     //crie rotator1 e adicione ao mundo
     rotator1 = Bodies.rectangle(250,200,150,20,rotator_options);
     World.add(world,rotator1);
     
     //crie rotator2 e adicione ao mundo
-    
-    //crie rotator3 e adicione ao mundo
-    
+    rotator2 = Bodies.rectangle(250,200,150,20,rotator_options);
+    World.add(world,rotator2);
+
+    //crrotator1 = Bodies.rectangle(250,200,150,20,rotator_options);
+    rotator3 = Bodies.rectangle(250,200,150,20,rotator_options);
+    World.add(world,rotator3);
 
     //estilizando os corpos aqui
     fill("brown");
@@ -75,10 +91,19 @@ function draw(){
   //Exibir plane e block1 e block2
   rect(plane.position.x,plane.position.y,1200,20);
   rect(block1.position.x,block1.position.y,150,20);
- 
+
+  rect(block2.position.x,block2.position.y,150,20);
 
   //Exibir forma para todas as partículas 1 a 5
   ellipse(particle1.position.x,particle1.position.y,10);
+
+  ellipse(particle2.position.x,particle2.position.y,10);
+
+  ellipse(particle3.position.x,particle3.position.y,10);
+
+  ellipse(particle4.position.x,particle4.position.y,10);
+
+  ellipse(particle5.position.x,particle5.position.y,10);
     
 
   //crie forma para todos os rotate1
@@ -91,10 +116,22 @@ function draw(){
   angle1 +=0.2;
   
   //crie forma para todos os rotate2
-    
+  Matter.Body.rotate(rotator2,angle2)
+  push();
+  translate(rotator2.position.x,rotator2.position.y);
+  rotate(angle2);
+  rect(0,0,150,20);
+  pop();
+  angle2 +=0.3;
   
   //crie forma para todos os rotate3
-
+  Matter.Body.rotate(rotator3,angle3)
+  push();
+  translate(rotator3.position.x,rotator3.position.y);
+  rotate(angle3);
+  rect(0,0,150,20);
+  pop();
+  angle3 +=0.4;
     
     
 }
